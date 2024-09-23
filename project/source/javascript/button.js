8a4c44e08a1c8742e7d504d81abe406c2c96e131
Jhon-vlfs <joao.vlfreitas@aluno.educacao.pe.gov.br>
2024-09-23 12:25:09 -0300

    primeiro commit

---
+++ project/source/javascript/button.js
@@ -0,0 +1,7 @@
+function mostrarConteudo() {
+    //* Exibe o conteúdo oculto
+    document.getElementById("conteudo-extra").style.display = "block";
+    
+    //! Remove o botão "Continue Lendo" após o clique
+    document.getElementById("continue-lendo").style.display = "none";
+} //TODO: depois eu poderia melhorar o script desse botão...
